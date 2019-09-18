95afd705e8f23a1b6e14d781261a63d8d6bfe9fb
vegamave <54917393+vegamave@users.noreply.github.com>
2019-09-18 16:40:53 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,24 @@
+var cafes = ["Tostado Frances", "Colombiano", "Knoa"];
+console.log (cafes); 
+document.write (cafes);
+var peces = ["Leon", , "Angel"];
+document.write (peces);
+var edad = 18;
+if (edad >=18) {
+alert ("Eres mayor de edad"); }
+// verdaddero 
+else {
+//falso 
+ 
+alert ("Todavía eres menor de edad"); } 
+var nombre = "";
+if (nombre == "") {
+	
+	alert ("Aun no nos has dicho tu nombre");
+    nombre = prompt ("ingrese su nombre");
+   console.log ("Gracias "+nombre+", hemos guardado sus datos.");
+   document.write ("Su nombre es: <b> "+nombre+" </b> <br>"); 
+   
+}
+else { ("Hemos guardado tu nombre"); 
+}
---
+++ js2.js
@@ -0,0 +1,61 @@
+   var texto="este es un texto";
+   var numero= "23,09";
+   var boleana="true";
+   
+   
+   document.write("<h1>Clase 2 JS Online</h1>")
+
+	console.log("la variable texto contiene "+texto+"<br>"); 
+	console.log("la variable numero vale "+numero+"<br>"); 
+	console.log("la variable boleana contiene "+boleana+"<br>");
+	
+	document.write("la variable texto contiene "+texto+"<br>"); 
+	document.write("la variable numero vale "+numero+"<br>"); 
+	document.write("la variable boleana contiene "+boleana+"<br>") 
+	
+	var texto="cambio de texto";
+   var numero= "1992";
+   var boleana="false";
+   
+   
+   document.write("<h1>Cambio de valor de variable</h1>")
+
+	console.log("la variable texto contiene "+texto+"<br>"); 
+	console.log("la variable numero vale "+numero+"<br>"); 
+	console.log("la variable boleana contiene "+boleana+"<br>");
+	
+	document.write("la variable texto contiene "+texto+"<br>"); 
+	document.write("la variable numero vale "+numero+"<br>"); 
+	document.write("la variable boleana contiene "+boleana+"<br>")
+	
+	var x="La respuesta es "+42;
+   var y= 42+ "es la respuesta";
+   var w="37"-7;
+   var z="37"+7;
+   
+   
+   document.write("<h1>Prueba de codigo</h1>")
+
+	console.log(x); 
+	document.write(""+x+"<br>");
+	
+	console.log(y); 
+	document.write(""+y+"<br>"); 
+	
+   console.log(w); 
+	document.write(""+w+"<br>"); 
+	
+   console.log(z); 
+	document.write(""+z+"<br>"); 
+	
+	var resultado=0;
+	resultado=w+7;
+	console.log(resultado); 
+	document.write("<b> El resultado de W+7 es"+resultado+"<b><br>");
+	
+	var cafes=['kona','Tostado frances','Colombiano'];
+	console.log(cafes); 
+	document.write(cafes);
+	
+	
+
